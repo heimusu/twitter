dde18709497f37c30380f468112663e29038bebc
久保 陽一郎 <a14192@pc-10597.cyberagent.co.jp>
2016-04-18 13:37:41 +0900

    :heavy_plus_sign: モックサーバーからのツイートデータ取得とTLとしての出力が完成

--- loginForm.tag
+++ loginForm.tag
@@ -1,7 +1,7 @@
 <loginForm>
     <div class='inputForm' style='text-align:center;'>
-        <input class='form-control' type='text' name='username' style='width:20%;' placeholder='ユーザーネーム'>
-        <input class='form-control' type='text' name='password' style='width:20%;' placeholder='パスワード'>
+        <input class='form-control' type='text' name='username' style='width:20%; margin:10px;' placeholder='ユーザーネーム'>
+        <input class='form-control' type='text' name='password' style='width:20%; margin:10px;' placeholder='パスワード'>
         <button class='btn' style="margin:10px;">ログイン</button>
     </div>
 </loginForm>
--- public/loginForm.js
+++ public/loginForm.js
@@ -1,2 +1,2 @@
-riot.tag2('loginform', '<div class="inputForm" style="text-align:center;"><input class="form-control" type="text" name="username" style="width:20%;" placeholder="ユーザーネーム"><input class="form-control" type="text" name="password" style="width:20%;" placeholder="パスワード"><button class="btn" style="margin:10px;">ログイン</button></div>', '', '', function(opts) {
+riot.tag2('loginform', '<div class="inputForm" style="text-align:center;"><input class="form-control" type="text" name="username" style="width:20%; margin:10px;" placeholder="ユーザーネーム"><input class="form-control" type="text" name="password" style="width:20%; margin:10px;" placeholder="パスワード"><button class="btn" style="margin:10px;">ログイン</button></div>', '', '', function(opts) {
 });
